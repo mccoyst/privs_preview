fdd3e83ee27636d6f86d4f7846bf583c2dd9022d
Steve McCoy <mccoyst@gmail.com>
2013-10-09 14:34:48 -0400

    Add a bug to the doc

--- doc.go
+++ doc.go
@@ -8,3 +8,5 @@ These functions are implemented with advice from "Setuid Demystified":
 http://www.cs.berkeley.edu/~daw/papers/setuid-usenix02.pdf
 */
 package privs
+
+//  BUG(mccoyst): The linux setuid system calls only affect the current thread; not the whole process. https://code.google.com/p/go/issues/detail?id=1435
